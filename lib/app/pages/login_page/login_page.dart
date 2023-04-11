6478fe4747d61a373a5150413cff8e36a2898e7d
sarpketenci <57857604+sarpketenci@users.noreply.github.com>
2023-04-11 15:55:36 +0300

    Base version for login page

--- lib/app/pages/login_page/login_page.dart
+++ lib/app/pages/login_page/login_page.dart
@@ -1,12 +1,70 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage ({Key ? key}):super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Login Page"),
-    );
+    return Scaffold(
+      //backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Pre - Assessment'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
+                child: Image.asset('assets/images/loginIcon.jpg')
+
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'ID',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              Container(
+                  height: 80,
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: const Text('Log In'),
+                    onPressed: () {},
+                  )),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Don't have an account ?",
+                  style: TextStyle(color: Colors.grey[600]),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
